2069849cb6475ff79b561a4cd337a6261de92905
BenedictUmeozor <benedictumeozor@gmail.com>
2023-08-07 13:38:31 +0100

    loader

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const addFormBtn = document.getElementById("add-form-btn");
 const closeFormBtn = document.getElementById("close-form-btn");
 const formContainer = document.querySelector(".add-form");
+const loader = document.querySelector(".loader");
 
 const showForm = () => {
-  formContainer.classList.add("show");
+  loader.classList.add("show");
+  setTimeout(() => {
+    loader.classList.remove("show");
+    formContainer.classList.add("show");
+  }, 1500);
 };
 
 const hideForm = () => {
+  loader.classList.add("show");
   formContainer.classList.remove("show");
+  setTimeout(() => {
+    loader.classList.remove("show");
+  }, 1500);
 };
 
 addFormBtn.addEventListener("click", showForm);
